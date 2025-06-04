83d55d701fdea9c8f0fe5ff6cb8c5e2f4ab34441
Vladimir Ivanov <volodimir1024@gmail.com>
2025-06-04 14:04:09 +0100

    added an async version of RemoteDockerSandbox

--- scalable_docker/client.py
+++ scalable_docker/client.py
@@ -18,7 +18,6 @@ class ProcessOutput:
 
 
 @beartype
-@dataclass
 class RemoteDockerSandbox(JsonRESTClient):
     def __init__(
         self,
@@ -95,6 +94,94 @@ class RemoteDockerSandbox(JsonRESTClient):
             filename=filename, content=content
         )
         return self.run_commands([command])[0]
+
+    @staticmethod
+    def upload_file_command(filename: str, content: str) -> str:
+        encoded_data = base64.b64encode(content.encode()).decode()
+        return f"echo {encoded_data} | base64 -d > {quote(filename)}"
+
+
+@beartype
+class AsyncRemoteDockerSandbox(JsonRESTClient):
+    container_name: str
+
+    @classmethod
+    async def make_sandbox(
+        cls,
+        dockerfile_content: str,
+        startup_commands: list[str] | None = None,
+        max_memory_gb: float | int | None = 1.0,
+        max_cpus: int | None = 1,
+        max_lifespan_seconds: int | None = 10_000,
+        server_url: str | None = None,
+    ) -> None:
+        if server_url is None:
+            server_url = os.environ.get("DOCKER_SANDBOX_SERVER_URL")
+
+        if server_url is None:
+            raise ValueError(
+                "A sever url must be provided to RemoteDockerSandbox, either by passing a `server_url` argument to its constructor or by setting the `DOCKER_SANDBOX_SERVER_URL` system variable."
+            )
+
+        sandbox = cls(server_url=server_url)
+
+        sandbox.container_name = f"sandbox-container-{uuid4()}"
+
+        creation_response = await sandbox.call_server(
+            function="create_sandbox",
+            container_name=sandbox.container_name,
+            dockerfile_content=dockerfile_content,
+            startup_commands=startup_commands if startup_commands is not None else [],
+            max_memory_gb=max_memory_gb,
+            max_cpus=max_cpus,
+            max_lifespan_seconds=max_lifespan_seconds,
+        )
+
+        if sandbox.server_response_is_error(creation_response):
+            raise ValueError(
+                f"Failed creating sandbox. The server response is: {creation_response}"
+            )
+
+    def server_response_is_error(self, response: Any) -> bool:
+        return isinstance(response, dict) and "error" in response.keys()
+
+    async def run_commands(
+        self,
+        commands: list[str],
+        total_timeout_seconds: float | int = 5.0,
+        per_command_timeout_seconds: float | int = 4.0,
+    ) -> list[ProcessOutput]:
+        response = await self.call_server(
+            function="run_commands",
+            container_name=self.container_name,
+            commands=commands,
+            total_timeout_seconds=total_timeout_seconds,
+            per_command_timeout_seconds=per_command_timeout_seconds,
+        )
+
+        if self.server_response_is_error(response):
+            raise ValueError(
+                f"Failed running commands in the sandbox. The server response is: {response}"
+            )
+
+        return [ProcessOutput(**output) for output in response]
+
+    async def cleanup(self) -> None:
+        response = await self.call_server(
+            function="cleanup_sandbox", container_name=self.container_name
+        )
+
+        if self.server_response_is_error(response):
+            raise ValueError(
+                f"Failed sandbox cleanup. The server response is: {response}"
+            )
+
+    async def upload_file(self, filename: str, content: str) -> ProcessOutput:
+        command = RemoteDockerSandbox.upload_file_command(
+            filename=filename, content=content
+        )
+        outputs = await self.run_commands([command])
+        return outputs[0]
 
     @staticmethod
     def upload_file_command(filename: str, content: str) -> str:
--- scalable_docker/rest_client_base.py
+++ scalable_docker/rest_client_base.py
@@ -1,4 +1,5 @@
 import requests
+import aiohttp
 import traceback
 from typing import Any
 from beartype import beartype
@@ -31,10 +32,46 @@ class JsonRESTClient:
             parsed_response = response.json()
         except requests.exceptions.JSONDecodeError:
             parsed_response = None
-        
+
         if response.status_code != 200:
             return {
                 "error": f"Error communicating with server.\nStatus code: {response.status_code}.\nResponse json: {parsed_response}"
             }
 
         return parsed_response
+
+
+@beartype
+class AsyncJsonRESTClient:
+    server_url: str
+
+    def __init__(self, server_url: str) -> None:
+        self.server_url = server_url
+
+    @property
+    def endpoint(self):
+        return f"{self.server_url}/process"
+
+    async def call_server(self, **kwargs) -> Any:
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.post(
+                    self.endpoint,
+                    json=kwargs,
+                    headers={"Content-Type": "application/json"},
+                ) as response:
+                    try:
+                        parsed_response = await response.json()
+                    except aiohttp.ContentTypeError:
+                        parsed_response = None
+
+                    if response.status != 200:
+                        return {
+                            "error": f"Error communicating with server.\nStatus code: {response.status}.\nResponse json: {parsed_response}"
+                        }
+
+                    return parsed_response
+        except Exception as e:
+            return {
+                "error": f"Error communicating with server: {e} {traceback.format_exc()}"
+            }
